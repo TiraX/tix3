865be38c8493d3251d59ada5444d2ed80801d555
TiraX <zhaoshuai2@kingsoft.com>
2022-11-11 18:31:34 +0800

    Change persistent cull thread group back to 1440.

--- Samples/01.NaniteLearning/PersistentCullCS.cpp
+++ Samples/01.NaniteLearning/PersistentCullCS.cpp
@@ -93,7 +93,7 @@ void FPersistentCullCS::Run(FRHICmdList* RHICmdList)
 	RHICmdList->SetComputeResourceTable(FPersistentCullCS::RT_Table, ResourceTable);
 	RHICmdList->DispatchCompute(
 		FInt3(NANITE_PERSISTENT_CLUSTER_CULLING_GROUP_SIZE, 1, 1),
-		FInt3(1, 1, 1)
+		FInt3(1440, 1, 1)
 	);
 	RHICmdList->EndEvent();
 	++frame;
